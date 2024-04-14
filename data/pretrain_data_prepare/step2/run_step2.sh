5736c178eec6752fa72d3256f42d5c2498dcf910
Li-Shu14 <lishu14@foxmail.com>
2024-04-14 04:42:41 +0000

    add requirement

---
+++ data/pretrain_data_prepare/step2/run_step2.sh
@@ -0,0 +1,6 @@
+DJ_PATH=/hoe/test/lishu/data-juicer
+CODE_YAML=steel_project_starcode.yaml
+TXT_YAML=steel_project_txt.yaml
+
+nohup python $DJ_PATH/tools/process_data.py --config $CODE_YAML > dj_log_code 2>&1 &
+nohup python $DJ_PATH/tools/process_data.py --config $TXT_YAML > dj_log_txt 2>&1 &
